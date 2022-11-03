e132e251cb61e9770b6a74af3f235bf71e55af4c
Jacob Moore <jlmoors001@gmail.com>
2022-11-03 17:00:03 -0500

    Humble updates to ros2-rebase-fix branch

--- vesc_ackermann/src/vesc_to_odom.cpp
+++ vesc_ackermann/src/vesc_to_odom.cpp
@@ -60,13 +60,13 @@ VescToOdom::VescToOdom(const rclcpp::NodeOptions & options)
   base_frame_ = declare_parameter("base_frame", base_frame_);
   use_servo_cmd_ = declare_parameter("use_servo_cmd_to_calc_angular_velocity", use_servo_cmd_);
 
-  speed_to_erpm_gain_ = declare_parameter("speed_to_erpm_gain").get<double>();
-  speed_to_erpm_offset_ = declare_parameter("speed_to_erpm_offset").get<double>();
+  declare_parameter("speed_to_erpm_gain", speed_to_erpm_gain_);
+  declare_parameter("speed_to_erpm_offset", speed_to_erpm_offset_);
 
   if (use_servo_cmd_) {
-    steering_to_servo_gain_ = declare_parameter("steering_angle_to_servo_gain").get<double>();
-    steering_to_servo_offset_ = declare_parameter("steering_angle_to_servo_offset").get<double>();
-    wheelbase_ = declare_parameter("wheelbase").get<double>();
+    declare_parameter("steering_angle_to_servo_gain", steering_to_servo_gain_);
+    declare_parameter("steering_angle_to_servo_offset", steering_to_servo_offset_);
+    declare_parameter("wheelbase", wheelbase_);
   }
 
   publish_tf_ = declare_parameter("publish_tf", publish_tf_);
